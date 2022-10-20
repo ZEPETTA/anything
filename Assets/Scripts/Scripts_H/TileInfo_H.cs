bc62880065360f2e676510612c7e877ef67fd13d
mynameisjinhohong <ghddhksduq@naver.com>
2022-10-20 15:18:29 +0900

    ㅌㅏㅇㅣㄹㄹㅗㄷㅡㅇㅗㅏㄴㅅㅓㅇ

--- Assets/Scripts/Scripts_H/TileInfo_H.cs
+++ Assets/Scripts/Scripts_H/TileInfo_H.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class TileInfo_H : MonoBehaviour
 {
-    TileInfo tileInfo;
+    public TileInfo tileInfo;
     // Start is called before the first frame update
     void Start()
     {
