7c89d0b0d4fe0c4cbf35626e86a85526df0435de
jinhoLeeee <9oo9leljh@gmail.com>
2022-10-29 13:32:43 +0900

    게시판 하는 중

---
+++ Assets/Scripts/Scripts_L/NoticeBoardManager_L.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NoticeBoardManager_L : MonoBehaviour
+{
+    public InputField inputPostit;
+
+
+    public void OnClickSubmitPostit()
+    {
+
+    }
+}
